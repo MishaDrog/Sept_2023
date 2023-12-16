eb770cbf3a39511e68f8ee23b77dfc8662193369
MishaDrog <drogmisha@gmail.com>
2023-12-16 16:21:26 +0200

    js_hw5

--- js/hw5/hw5.js
+++ js/hw5/hw5.js
@@ -10,6 +10,21 @@ function rectangle(a,b) {
 console.log(rectangle(25, 37));
 
 
+
+
+let footballPlayers = [
+    {id: 1, name: 'Heymar jr', age: 31, status: 'online'},
+    {id: 2, name: 'Messi', age: 35, status: 'ofline'},
+    {id: 3, name: 'C.Ronaldo', age: 37, status: 'online'},
+    {id: 4, name: 'Mudryk', age: 25, status: 'ofline'},
+    {id: 5, name: 'Lewandowski', age: 30, status: 'online'},
+    {id: 6, name: 'Kross', age: 35, status: 'ofline'},
+    {id: 7, name: 'Benzema', age: 34, status: 'online'},
+    {id: 8, name: 'Mbape', age: 26, status: 'online'},
+    {id: 9, name: 'Militao', age: 25, status: 'ofline'},
+    {id: 10, name: 'Vinicius jr', age: 23, status: 'ofline'},
+];
+
 // стрілочна ф-я;
 const rectangle2 = (a,b) =>  {
     return a+b;
@@ -43,6 +58,18 @@ console.log(sCel(23,34));
 /*
 - створити функцію яка приймає масив та виводить кожен його елемент
  */
+
+const fnArr = function (arr) {
+    for (const item of arr) {
+        console.log(item);
+
+    }
+
+};
+
+
+
+fnArr(footballPlayers);
 
 /*
 - створити функцію яка створює параграф з текстом. Текст задати через аргумент
@@ -82,6 +109,7 @@ console.log(sCel(23,34));
 - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
  */
+
 
 /*
 - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
